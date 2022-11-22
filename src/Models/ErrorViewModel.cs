df7cb566e3aed623e8af177fdd2eb57ccb89ec10
iaspnetcore <290996724@qq.com>
2022-11-22 12:16:50 +0800

    update from .net 6 to .net 7

--- src/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Miniblog.Core.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
