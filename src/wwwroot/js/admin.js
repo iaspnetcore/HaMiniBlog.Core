4a7c0372d638f159125112ac1881e550426b34cf
iaspnetcore <290996724@qq.com>
2022-12-04 15:50:29 +0800

    上传原版 .net 7.0

--- src/wwwroot/js/admin.js
+++ src/wwwroot/js/admin.js
@@ -68,7 +68,7 @@
                 autoresize_min_height: 200,
                 plugins: 'autosave preview searchreplace visualchars image link media fullscreen code codesample table hr pagebreak autoresize nonbreaking anchor insertdatetime advlist lists textcolor wordcount imagetools colorpicker',
                 menubar: "edit view format insert table",
-                toolbar1: 'formatselect undo redo code codesample | bold italic blockquote forecolor backcolor | imageupload link | alignleft aligncenter alignright  | numlist bullist outdent indent | fullscreen',
+                toolbar1: 'formatselect | bold italic blockquote forecolor backcolor | imageupload link | alignleft aligncenter alignright  | numlist bullist outdent indent | fullscreen',
                 selection_toolbar: 'bold italic | quicklink h2 h3 blockquote',
                 autoresize_bottom_margin: 0,
                 paste_data_images: true,
